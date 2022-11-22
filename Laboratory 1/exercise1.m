1dfec854e106023f990ee64bfb4997c2649873c5
CalvezJohnPatrick <113434610+CalvezJohnPatrick@users.noreply.github.com>
2022-11-22 21:20:52 +0800

    Add files via upload

--- Laboratory 1/exercise1.m
+++ Laboratory 1/exercise1.m
@@ -5,18 +5,18 @@ clc
 pkg load image;
 
 Orig_Img = imread('fruits.png');
-subplot(231);
+subplot(131);
 imshow(Orig_Img);
 title('Original Image')
 
 Orig_Img1 = imresize(imresize(Orig_Img,1/16),16);
-subplot(232);
+subplot(132);
 imshow(Orig_Img1);
 title('16x16 Resolution');
 imwrite(Orig_Img1,'fruits2.png');
 
 Chg_Color = rgb2hsv(Orig_Img);
-subplot(233)
+subplot(133)
 imshow(Chg_Color);
 title('RGB to HSV');
 imwrite(Chg_Color,'fruits3.png');
